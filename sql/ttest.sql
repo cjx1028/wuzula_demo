b053bc940df4996a1048ab82336226fb475d3a9c
cjx1028 <cjx1028@gmail.com>
2016-05-17 15:29:27 +0800

    添加ttest脚本说明

--- sql/ttest.sql
+++ sql/ttest.sql
@@ -2,7 +2,8 @@ create table ttest(
 id number(5),
 name char(20)
 )
-
+                     
+--必须要创建unique索引才可以使用CDaoUtils中的find功能
 drop index idx_ttest;
 create unique index idx_ttest on  ttest (
 id
